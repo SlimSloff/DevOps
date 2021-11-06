e6ab643f19803b7151d2fbdcdd1e60053701218a
SlimSloff <slim.barakati@esprit.tn>
2021-11-06 16:15:38 +0100

    entities added

---
+++ src/main/java/tn/esprit/spring/HelloController.java
@@ -0,0 +1,16 @@
+package tn.esprit.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello From SpringBoot";
+	}
+	
+	
+}
+
